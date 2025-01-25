531e2c8b83807cb1b6948837635e21c563a5c531
SooYeonKim <lovesu5632@gmail.com>
2025-01-25 16:57:54 +0900

    feat: Nuxt plugins(다국어)

--- learn-nuxt-3/nuxt.config.ts
+++ learn-nuxt-3/nuxt.config.ts
@@ -10,4 +10,12 @@ export default defineNuxtConfig({
   quasar: {
     /* */
   },
+  imports: {
+    presets: [
+      {
+        from: 'vue-i18n',
+        imports: ['useI18n'],
+      },
+    ],
+  },
 });
