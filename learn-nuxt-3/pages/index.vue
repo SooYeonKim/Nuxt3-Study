5b5adc3e1464d2a197bb876585922c47c5afbed6
SooYeonKim <lovesu5632@gmail.com>
2025-02-06 19:59:48 +0900

    feat: Client Data fetching - Course API

--- learn-nuxt-3/pages/index.vue
+++ learn-nuxt-3/pages/index.vue
@@ -26,7 +26,7 @@
 console.log('index.vue');
 </script>
 <script setup lang="ts">
-const { courses } = useCourses();
+const { courses } = await useCourses();
 </script>
 
 <style lang="scss" scoped></style>
